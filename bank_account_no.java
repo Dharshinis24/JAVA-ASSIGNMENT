48d78b9cb58d998b3382ae0ea25a1ab8d56449fa
Dharshinis24 <131352271+Dharshinis24@users.noreply.github.com>
2023-10-17 06:55:08 +0530

    Add files via upload

---
+++ bank_account_no.java
@@ -0,0 +1,112 @@
+public class bank_account_no{
+ 
+       int acc_no,balance = 0;
+  
+       bank_account_no(int x){
+      
+       acc_no = x;
+  
+      }
+   
+     void depositing(int x){
+        
+        balance += x;
+       
+       balance_details();
+   
+     }
+   
+   void Withdrawing(int x){
+       
+       if(balance != 0){
+            
+               if(balance < x){
+            
+                         System.out.println("Account Number:"+acc_no+"\nInsufficient Amount!");
+           
+               }
+            
+             else{
+            
+                  balance -= x;
+            
+                 balance_details();
+            
+             }
+       
+        }
+      
+       else
+       
+       System.out.println("Account Number:"+acc_no+"\nYour Balance is Zero,Cann't Withdraw");
+    
+      }
+    
+   void balance_details(){
+       
+              System.out.println("Account Number:"+acc_no+"\nBalance:"+balance);
+    
+    }
+
+public static void main(String[] args){
+    
+bank_account_no obj1 = new bank_account_no(1945);
+ 
+bank_account_no obj2 = new bank_account_no(2000);
+    
+bank_account_no obj3 = new bank_account_no(2999);
+    
+obj1.depositing(5000);
+   
+obj2.depositing(10000);
+    
+obj2.Withdrawing(2000);
+    
+obj3.depositing(2000);
+    
+obj1.Withdrawing(2500);
+    
+obj3.Withdrawing(20000);
+    
+obj1.Withdrawing(2500);
+   
+ obj1.Withdrawing(2500);
+   
+ 
+}
+
+}
+
+OUTPUT:
+
+Account Number:1945
+
+Balance:5000
+
+Account Number:2000
+
+Balance:10000
+
+Account Number:2000
+
+Balance:8000
+
+Account Number:2999
+
+Balance:2000
+
+Account Number:1945
+
+Balance:2500
+
+Account Number:2999
+
+Insufficient Amount!
+
+Account Number:1945
+
+Balance:0
+
+Account Number:1945
+
+Your Balance is Zero,Cann't Withdraw
---
+++ vending_machine (1).java
@@ -0,0 +1,114 @@
+import java.util.Dictionary;
+
+import java.util.Enumeration;
+
+import java.util.Hashtable;
+
+public class vending_machine{
+   
+        int amount=0,balance=0;
+    
+       void items(Dictionary dictionary){
+     
+                     Enumeration<String> keys = dictionary.keys();
+		
+                     while (keys.hasMoreElements()) {
+		 
+                                   for(int i=0;i<dictionary.size();i++){
+			
+                                              String key = keys.nextElement();
+			
+                                              System.out.println(key);
+     
+                                   }
+		
+                     }
+    
+          }
+    
+       void dispense(int op){
+        
+                      if(op == 1)
+            
+                             System.out.println("Dispensing lassi");
+       
+                      if(op == 2)
+            
+                             System.out.println("Dispensing biscuits");
+        
+                      if(op == 3)
+            
+                              System.out.println("Dispensing chips");
+   
+        }
+    
+       void calculate_transaction(int VA,int q){
+       
+                   amount = (q) * (VA);
+        
+                  System.out.println("Your Amount = "+amount);
+   
+       }
+
+      void change(int x){
+           
+               balance = x - amount;
+          
+               System.out.println("Your change = "+balance);
+     
+      }
+public static void main(String[] args) {
+		
+                        Dictionary<String, Integer> dictionary = new Hashtable<>();
+		
+                       dictionary.put("1.lassi", 100);
+		
+                       dictionary.put("2.biscuits", 200);
+		
+                       dictionary.put("3.chips", 300);
+		
+                       vending_machine obj1 = new vending_machine();
+		
+                       int v1 = dictionary.get("1.lassi");
+		
+                       int v2 = dictionary.get("2.biscuits");
+		
+                       int v3 = dictionary.get("3.chips");
+		
+                       obj1.items(dictionary);
+		
+                       obj1.dispense(2);
+		
+                       obj1.calculate_transaction(v2,1);
+	
+                       obj1.change(500);	
+                       obj1.dispense(1);
+		
+                       obj1.calculate_transaction(v1,4);
+
+	     obj1.change(500);
+}	
+
+}
+
+OUTPUT:
+
+3.chips
+
+1.lassi
+
+2.biscuits
+
+Dispensing biscuits
+
+Your Amount = 200
+
+Your change = 300
+
+Dispensing lassi
+
+Your Amount = 400
+
+Your change = 100
+
+
